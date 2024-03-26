30daea88588bb41131fe9877399ebbe5ee48b9bb
효석 이 <lhs19960330@gmail.com>
2024-03-26 19:39:34 +0900

    feat : 뻘짓함

---
+++ Assets/TestCoding.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// 이효석이 만든 코드
+public class TestCoding : MonoBehaviour
+{
+    private GameObject my;
+    // Start is called before the first frame update
+    void Start()
+    {
+        my = FindObjectOfType<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
